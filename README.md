0f15b4b93000e9ca48de6673f983a7ef5244dfe8
mingchen0919 <ming@cryoocyte.com>
2019-04-24 15:33:21 -0400

    update

--- README.md
+++ README.md
@@ -1,1 +1,49 @@
+# Template
 
+Install **cookiecutter** if you do not have it.
+
+```bash
+pip install cookiecutter
+```
+
+Clone this repository to the system temporary folder `$TMPDIR`.
+
+```bash
+git clone https://github.com/MingChen0919/pyproject_template.git $TMPDIR/pyproject_template
+```
+
+
+Go to the folder where you would like to store the project and create the project template.
+
+```bash
+cd <folder_to_store_project>
+cookiecutter $TMPDIR/pyproject_template
+```
+
+Create a virtual environment and install the package.
+
+```bash
+cd <project_name>
+mkdirtualenv <project_name>
+pip install -e .
+pip freeze | grep -v <package_name> > requirements.txt
+git init
+git add .
+git commit -m "First commit"
+git remote add origin <remote_repository_url>.git
+git push -u origin master
+```
+
+Moreover, if you are planning to use the Jupyter Notebook, you have to install the kernel of the environment.
+
+``
+python -m ipykernel install --user --name <project_name>
+``
+
+Now, create a branch, switch to it,
+
+```bash
+git checkout -b <branch_name>
+```
+
+and you are ready! 
--- setup.py
+++ setup.py
@@ -1,27 +1,37 @@
 import setuptools
 
-with open("README.md", "r") as fh:
-	long_description = fh.read()
+def readme():
+    """Import the README.md Markdown file and try to convert it to RST format."""
+    try:
+        import pypandoc
+        return pypandoc.convert('README.md', 'rst')
+    except(IOError, ImportError):
+        with open('README.md') as readme_file:
+            return readme_file.read()
 
 setuptools.setup(
-	name="project_name",
-	version="0.0.1",
-	author="Ming Chen",
-	author_email="mingchen@example.com",
-	description="A small example package",
-	long_description=long_description,
-	long_description_content_type="text/markdown",
-	url="https://github.com/pypa/project_name",
-	packages=setuptools.find_packages(),
+	name='{{cookiecutter.package_name}}',
+	version='{{cookiecutter.package_version}}',
+	description='{{cookiecutter.package_short_description}}',
+	long_description=readme(),
 	classifiers=[
-		"Programming Language :: Python :: 3",
-		"License :: OSI Approved :: MIT License",
-		"Operating System :: OS Independent",
+		'Programming Language :: Python :: 3',
 	],
+	url='{{cookiecutter.project_url}}',
+	author='{{cookiecutter.author_name}}',
+	author_email='{{cookiecutter.author_email}}',
+	license='{{cookiecutter.package_license}}',
+	packages=['{{cookiecutter.package_name}}'],
 	install_requires=[
 		'pypandoc>=1.4',
+		'watermark>=1.8.1',
+		'pandas>=0.24.2',
+		'scikit-learn>=0.20.3',
+		'scipy>=1.2.1',
+		'matplotlib>=3.0.3',
 		'pytest>=4.3.1',
 		'pytest-runner>=4.4',
+		'click>=7.0'
 	],
 	setup_requires=['pytest-runner'],
 	tests_require=['pytest'],
