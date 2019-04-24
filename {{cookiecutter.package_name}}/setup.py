b50861efddbe465d450ccfa14e2ed4006f6f23e4
mingchen0919 <ming@cryoocyte.com>
2019-04-24 17:20:48 -0400

    update

--- {{cookiecutter.package_name}}/setup.py
+++ {{cookiecutter.package_name}}/setup.py
@@ -4,7 +4,7 @@ def readme():
     """Import the README.md Markdown file and try to convert it to RST format."""
     try:
         import pypandoc
-        return pypandoc.convert('README.md', 'rst')
+        return pypandoc.convert_file('README.md', 'rst')
     except(IOError, ImportError):
         with open('README.md') as readme_file:
             return readme_file.read()
